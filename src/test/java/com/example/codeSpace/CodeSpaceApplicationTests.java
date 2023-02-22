4ff9907d0e1db8b94c02d7ecf8442fea23715a52
laxmiprasanna.veerapureddy <laxmiprasanna.veerapureddy@kotak.com>
2023-02-22 12:31:22 +0530

    first commit

---
+++ src/test/java/com/example/codeSpace/CodeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
